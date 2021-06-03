0311cd59d62b8439dad4a4995f2f9df435d6bfe7
R0rt1z2 <rogerortizleal@gmail.com>
2021-06-03 18:30:50 +0200

    laser_comm_arduino: v2: general cleanup
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- src/ino/v2/laser/laser.ino
+++ src/ino/v2/laser/laser.ino
@@ -1,38 +1,39 @@
-// LCD library
 #include <LiquidCrystal.h>
 
-// LCD Display
+/* Power ON LCD display */
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7); 
 
-/* Test String */
+/* Input characters */
 String input_buffer;
 
 /* Array Length */
 int array_size = 31;
 
-/* Arrays */
+/* Hardcoded arrays (for parsing) */
 String alphabet[] = {"H", "G", "N", "L", "X", "C", "W", "E", "D", "A", "Z", ".", "V", "Y", "R", "O", "J", "B", "Q", "I", "T", "K", "M", "P", "S", "F", "U", ",", "F", "W", " "};
 String sequences[] = {"33133", "13111", "31113", "13131", "11311", "11113", "11333", "31313", "11331", "33113", "11131", "11111", "33313", "33111", "31111", "31131", "13113", "31331", "11313", "13313", "11133", "13133", "31333", "13333", "33131", "13331", "33331", "13311", "33311", "31311", "33333"};
 
-/* Sequence delimiter */
-#define DELIMITER "-" // temp char until we figure out how to completely delete '\0's
-
 void setup() {
-  pinMode(11, OUTPUT); // DL
-  lcd.begin(10, 2); // LCD
-  digitalWrite(11, LOW); // turn off laser by default
-  Serial.begin(9600); // turn on serial
+  /* Open the serial port */
+  Serial.begin(9600);
+
+  /* Set up laser diode */
+  pinMode(11, OUTPUT);
+
+  /* Set up LCD display */
+  lcd.begin(10, 2);
+
+  /* Turn off laser by default */
+  digitalWrite(11, LOW);
 }
 
 String c2s(String c) {
-  for (int i = 0; i < array_size; i++) {
-    if (c.indexOf(alphabet[i]) >= 0) {
-      return sequences[i];
-    }
-  }
+  /* Converts single-characters to sequences */
+  for (int i = 0; i < array_size; i++) if (c.indexOf(alphabet[i]) >= 0) return sequences[i];
 }
 
 void s2l(String s) {
+  /* Sends given sequence through laser diode */
   for(auto x : s)
   {
     int b = (x == '1') ? 1 : 0;
@@ -42,34 +43,50 @@ void s2l(String s) {
 }
 
 void loop() {
+  /* Read user input data */
   while (Serial.available()) {
     delay(2);
     char c = Serial.read();
+
+    /* Skip NULL characters (useless crap) */
     if (c != "" || c != '\0') { 
       input_buffer += c;
     }
   }
   
+  /* Parse user input data (if it's not empty) */
   if (input_buffer != "") {
+    /* Send start frame */
     digitalWrite(11, HIGH);
     delay(1);
     digitalWrite(11, LOW);
+
     Serial.println(c2s(input_buffer));
-    if (input_buffer.length() ==  2) {
-      Serial.print("short");
+
+    /* Parse one character */
+    if (input_buffer.length() == 2) {
       s2l(c2s(input_buffer));
     } else {
+      /* Parse multiple characters */
       for (int i = 0; i < input_buffer.length() - 1; i++) {
         Serial.println(input_buffer[i]);
         s2l(c2s(String(input_buffer[i])));
+        /*
+        * NOTE: DON'T TOUCH ME
+        */
         delay(500);
+
+        /* Send start frame (again) */
         digitalWrite(11, HIGH);
         delay(1);
         digitalWrite(11, LOW);
       }
     }
 
+    /* Clean up user input data */
     input_buffer = "";
+
+    /* Turn off the laser diode */
     digitalWrite(11, LOW);
   }
 }
--- src/ino/v2/ldr/ldr.ino
+++ src/ino/v2/ldr/ldr.ino
@@ -1,49 +1,74 @@
 #include <string.h>
 #include <LiquidCrystal.h>
 
-// LCD Display
+/* Power ON LCD display */
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7); 
 
-// DL parser
+/* Light limit (for DL) */
 const int limit = 1000;
+
+/* Concatenated input bits */
 String input;
 
-// Arrays
+/* Start bit */
+int start_bit = 0;
+
+/* Hardcoded arrays (for parsing) */
 String alphabet[] = {"H", "G", "N", "L", "X", "C", "W", "E", "D", "A", "Z", ".", "V", "Y", "R", "O", "J", "B", "Q", "I", "T", "K", "M", "P", "S", "F", "U", ",", "F", "W", " "};
 String sequences[] = {"33133", "13111", "31113", "13131", "11311", "11113", "11333", "31313", "11331", "33113", "11131", "11111", "33313", "33111", "31111", "31131", "13113", "31331", "11313", "13313", "11133", "13133", "31333", "13333", "33131", "13331", "33331", "13311", "33311", "31311", "33333"};
 
 String s2c(String s) {
+  /* Converts sequences to single-characters */
   for (int i = 0; i < 31; i++) if (s == sequences[i]) return alphabet[i];
 }
 
 void setup() {
-  Serial.begin(9600); // open serial
-  lcd.begin(10, 2); // LCD
-  pinMode(A5, INPUT); // LDR
+  /* Open the serial port */
+  Serial.begin(9600);
+
+  /* Set up LCD display */
+  lcd.begin(10, 2);
+
+  /* Set up LDR */
+  pinMode(A5, INPUT);
 }
 
 void loop() {
-   int start_bit = 0;
-   
-   while (start_bit < limit) {
-      start_bit = analogRead(A5);
-   }
+  /* Wait for start sequence */
+  while (start_bit < limit) {
+    start_bit = analogRead(A5);
+  }
 
-   start_bit = 0;
-   delay(2);
-   
-   while (start_bit != -1) 
-   {
-      int output = (analogRead(A5) > limit) ? 1 : 3;
-      input += String(output);
-      start_bit += 1;
-      if (start_bit == 5)
-      {
-        lcd.print(s2c(input));
-        start_bit = -1;
-        input = "";
-      }
-      delay(500);
-   }
-   delay(1.5);
+  /* Reset the bit counter */
+  start_bit = 0;
+  delay(2);
+  
+  /* Loop over start_bit */
+  while (start_bit != -1) 
+  {
+    /* Read laser data (check > 1000) */
+    int output = (analogRead(A5) > limit) ? 1 : 3;
+
+    /* Concatenate sequence/bits */
+    input += String(output);
+    start_bit += 1;
+
+    /* Parse sequences */
+    if (start_bit == 5)
+    {
+      lcd.print(s2c(input));
+      start_bit = -1;
+      input = "";
+    }
+
+    /*
+     * NOTE: DON'T TOUCH ME
+     */
+    delay(500);
+  }
+
+  /*
+  * NOTE: DON'T TOUCH ME
+  */
+  delay(1.5);
 }
